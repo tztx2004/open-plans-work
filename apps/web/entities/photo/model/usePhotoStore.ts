8074aded87a8e876b455aba368cd292366f1da62
tztx2004 <tztx2004@gmail.com>
2026-01-16 23:16:10 +0900

    데이터 새로고침시 상태유지-zustand

--- apps/web/entities/photo/model/usePhotoStore.ts
+++ apps/web/entities/photo/model/usePhotoStore.ts
@@ -1,19 +1,28 @@
 'use client';
 
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import { GetPhotosResponse } from '../api/getPhotos.types';
 
 type PhotoStore = GetPhotosResponse & {
   setState: (photo: GetPhotosResponse) => void;
 };
 
-export const usePhotoStore = create<PhotoStore>((set) => ({
-  id: '',
-  author: '',
-  width: 0,
-  height: 0,
-  url: '',
-  download_url: '',
+export const usePhotoStore = create<PhotoStore>()(
+  persist(
+    (set) => ({
+      id: '',
+      author: '',
+      width: 0,
+      height: 0,
+      url: '',
+      download_url: '',
 
-  setState: (photo) => set(photo),
-}));
+      setState: (photo) => set(photo),
+    }),
+    {
+      name: 'photo-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
--- apps/web/entities/photo/ui/ImageBox.tsx
+++ apps/web/entities/photo/ui/ImageBox.tsx
@@ -4,15 +4,15 @@ import Image from 'next/image';
 import { usePhotoStore } from '../model/usePhotoStore';
 
 export default function ImageBox() {
-  const { download_url } = usePhotoStore();
+  const { download_url, width, height } = usePhotoStore();
 
   return (
     <article className='lg:pr-[20px] lg:flex-1'>
       <Image
         src={download_url}
         alt='result'
-        width={5000}
-        height={3333}
+        width={width}
+        height={height}
         className='w-full h-auto lg:h-full object-cover rounded-[24px]'
       />
     </article>
