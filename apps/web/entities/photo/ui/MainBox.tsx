6ed8908fb2354a0e4e353ea4e0a3bb8d845de902
tztx2004 <tztx2004@gmail.com>
2026-01-17 12:38:44 +0900

    lcp 배경 늦게뜨는 현상 수정

--- apps/web/entities/photo/ui/MainBox.tsx
+++ apps/web/entities/photo/ui/MainBox.tsx
@@ -1,18 +1,28 @@
 'use client';
 
+import Image from 'next/image';
 import { usePhotoStore } from '../model/PhotoCreateStore';
 
 export default function MainBox({ children }: { children: React.ReactNode }) {
   const { download_url } = usePhotoStore((state) => state);
 
   return (
-    <main
-      className='min-h-screen bg-cover bg-center bg-no-repeat flex flex-col'
-      style={{
-        backgroundImage: `url('${download_url}')`,
-        backdropFilter: 'blur(100px)',
-      }}
-    >
+    <main className='relative min-h-screen flex flex-col overflow-hidden'>
+      {download_url && (
+        <>
+          <Image
+            src={download_url}
+            alt='background'
+            fill
+            priority
+            className='object-cover -z-10'
+          />
+          <div
+            className='absolute inset-0 -z-10 backdrop-blur-[100px] bg-black/10'
+            aria-hidden='true'
+          />
+        </>
+      )}
       {children}
     </main>
   );
